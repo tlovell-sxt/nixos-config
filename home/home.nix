8bd6ca1027c0dbe58ee7944586d27c566c186208
Trevor Lovell <trevor.lovell@makeinfinite.com>
2026-01-14 20:53:25 +0000

    feat: add initial config with home manager

---
+++ home/home.nix
@@ -0,0 +1,35 @@
+{ config, pkgs, lib, ... }:
+
+let
+  home-manager = builtins.fetchTarball https://github.com/nix-community/home-manager/archive/release-25.05.tar.gz;
+in
+{
+  imports =
+    [
+      (import "${home-manager}/nixos")
+    ];
+
+  users.users.nixos.isNormalUser = true;
+  home-manager.users.nixos = { pkgs, ... }: {
+    programs.bash = {
+      enable = true;
+      bashrcExtra = ''
+        set -o vi
+      '';
+    };
+
+    programs.git = {
+      enable = true;
+      userName = "Trevor Lovell";
+      userEmail = "trevor.lovell@makeinfinite.com";
+    };
+
+    programs.ripgrep.enable = true;
+
+    programs.gh.enable = true;
+
+    # The state version is required and should stay at the version you
+    # originally installed.
+    home.stateVersion = "25.05";
+  };
+}
