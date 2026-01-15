2c5b39f97256041e24129b3e515e9d176d56a218
Trevor Lovell <trevor.lovell@makeinfinite.com>
2026-01-15 02:30:53 +0000

    feat: add experimental-features

--- system.nix
+++ system.nix
@@ -3,6 +3,11 @@
   wsl.enable = true;
   wsl.defaultUser = "nixos";
 
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
   environment.systemPackages = with pkgs; [
     git
     neovim
