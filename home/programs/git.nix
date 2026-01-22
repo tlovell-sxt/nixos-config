fa76276275027df64230057a7fbc397164852fe7
Trevor Lovell <trevor.lovell@makeinfinite.com>
2026-01-22 18:57:29 +0000

    feat: ignore direnv files in git

--- home/programs/git.nix
+++ home/programs/git.nix
@@ -5,8 +5,12 @@
     enable = true;
     settings.user.name = me.gitUserName;
     settings.user.email = me.gitUserEmail;
+
+    # direnv files, don't want to commit these
+    ignores = [".envrc" ".direnv"];
   };
 
+  # diffs that look more like github
   programs.delta = {
     enable = true;
     enableGitIntegration = true;
